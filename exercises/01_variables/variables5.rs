731055bddede26799fdea7f07f2ae43aa5a84656
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-09-23 19:12:58 -0700

    Finished lesson 2

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut number: &str = "T-H-R-E-E"; // Don't change this line
+    let number: &str = "T-H-R-E-E"; // Don't change this line
     println!("Spell a number: {}", number);
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
